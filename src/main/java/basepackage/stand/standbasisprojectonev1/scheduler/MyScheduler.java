6dd9f1be570b6d284a8c3e35c7ca469019a87dec
loyisoemma <loyisoemma@gmail.com>
2023-07-18 12:25:49 +0100

    July 18th findings

--- src/main/java/basepackage/stand/standbasisprojectonev1/scheduler/MyScheduler.java
+++ src/main/java/basepackage/stand/standbasisprojectonev1/scheduler/MyScheduler.java
@@ -217,7 +217,7 @@ public class MyScheduler {
 	// "0 0 0 * * 0" -- once a week
 	// 0 0 0 ? * WED
 	//@SuppressWarnings("deprecation")
-	@Scheduled(cron = "0 25 11 ? * TUE")
+	@Scheduled(cron = "0 35 11 ? * TUE")
     public void insertLessonnotes() {
 		
 		 Map<Integer, String> classMap = new HashMap<>();
