214437710bf3871b38dc40e377c314f157d9c411
loyboy <loyboy606@gmail.com>
2024-10-01 07:43:28 +0100

    change to sceduler 2

--- src/main/java/basepackage/stand/standbasisprojectonev1/scheduler/MyScheduler.java
+++ src/main/java/basepackage/stand/standbasisprojectonev1/scheduler/MyScheduler.java
@@ -294,7 +294,7 @@ public class MyScheduler {
 	
 	// "0 0/10 * * * *" - 10 minutes interval
 	// "0 0 1 * * *" - Everyday at 1:00 WAT 
-	@Scheduled(cron = "0 40 7 * * *")
+	@Scheduled(cron = "0 50 7 * * *")
     public void insertAttendances() {       
 		
 		//Check what day of the week is this
@@ -342,7 +342,7 @@ public class MyScheduler {
 	// 0 0 0 ? * WED fjfjfjfjfj
 	// 35 * * 31 11 * default
 	//@SuppressWarnings("deprecation")
-	@Scheduled(cron = "35 40 7 * * *")
+	@Scheduled(cron = "35 50 7 * * *")
     public void insertLessonnotes() {
 		
 		 Map<Integer, String> classMap = new HashMap<>();
