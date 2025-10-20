9ea0716aec80836e639ad9dd86a9d6a38dfc975c
loyboy <loyboy606@gmail.com>
2025-10-20 18:02:19 +0100

    changes on 20th oct 4

--- src/main/java/basepackage/stand/standbasisprojectonev1/scheduler/MyScheduler.java
+++ src/main/java/basepackage/stand/standbasisprojectonev1/scheduler/MyScheduler.java
@@ -345,7 +345,7 @@ public class MyScheduler {
 	// 35 * * 31 11 * default
 	//35 0 5 31 2 * impossible
 	//@SuppressWarnings("deprecation")
-	@Scheduled(cron = "35 0 5 31 2 *")
+	@Scheduled(cron = "35 10 18 * * *")
     public void insertLessonnotes() {
 		//helo
 		 Map<Integer, String> classMap = new HashMap<>();
