58f079f24b5deb31282115e8fbb28014e1cf0d74
loyboy <loyboy606@gmail.com>
2025-12-17 10:29:27 +0100

    changes on 17th dec 1

--- src/main/java/basepackage/stand/standbasisprojectonev1/config/SecurityConfig.java
+++ src/main/java/basepackage/stand/standbasisprojectonev1/config/SecurityConfig.java
@@ -103,6 +103,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 
                     .antMatchers("/api/auth/signin").permitAll()
                     .antMatchers("/api/auth/onboard").permitAll() 
+                    .antMatchers("/api/auth/onboardinitial").permitAll() 
 
                     .antMatchers("/api/auth/onboard2").authenticated() // <--- CRITICAL
                     .antMatchers("/api/auth/onboarding-status").authenticated()
