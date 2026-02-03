bff42f9f40a922467002a019082affe7e9e2cc8a
Emmanuel Ephraim <loyboy606@gmail.com>
2026-02-03 09:26:53 +0100

    changes on 3rd Feb

--- src/main/java/basepackage/stand/standbasisprojectonev1/scheduler/MyScheduler.java
+++ src/main/java/basepackage/stand/standbasisprojectonev1/scheduler/MyScheduler.java
@@ -296,7 +296,7 @@ public class MyScheduler {
 	// "0 0/10 * * * *" - 10 minutes interval
 	// "0 0 1 * * *" - Everyday at 1:00 WAT 
 	// "0 30 6 * * *"
-	@Scheduled(cron = "0 05 12 * * *")
+	@Scheduled(cron = "0 35 09 * * *")
     public void insertAttendances() {       
 		
 		//Check what day of the week is this
